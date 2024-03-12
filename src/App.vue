497ec2b878b1097380cb700757e2b3447569552b
Jayden <jayden.jang@tempestcgi.com>
2024-03-12 17:30:16 +0900

    first push

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <main id="main-tag">
+  </main>
+</template>
+
+<script>
+
+
+import TempestWidget from "@tempestcgi/tempest-widget";
+
+export default {
+  name: 'App',
+  components: {
+
+  },
+  mounted() {
+    const dataForInject = {
+      canvasId: 'application-canvas',
+      assetPrefix: 'https://showcase.tempest-vr.com/casper/',
+      debug: false,
+      tools: { sceneMode: true, cameraPosition: true },
+      canvasParentId: 'main-tag',
+    };
+
+    TempestWidget.widget(dataForInject);
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
